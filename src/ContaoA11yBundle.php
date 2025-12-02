1eb9b2fe06515ee5cc7f19a776d60efe8f435ef4
Rashid Bairamov <rashid@s-punkt-online.de>
2025-12-02 08:38:45 +0100

    Register services via loadExtension (backend menu listener)

--- src/ContaoA11yBundle.php
+++ src/ContaoA11yBundle.php
@@ -2,18 +2,20 @@
 
     declare(strict_types=1);
 
-    /*
-     * This file is part of weba11y/contao-a11y-bundle.
-     *
-     * (c) Rashid Bairamov
-     *
-     * @license LGPL-3.0-or-later
-     */
-
     namespace Weba11y\ContaoA11yBundle;
 
+    use Symfony\Component\DependencyInjection\ContainerBuilder;
+    use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
     use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
     class ContaoA11yBundle extends AbstractBundle
     {
+        public function loadExtension(
+            array $config,
+            ContainerConfigurator $containerConfigurator,
+            ContainerBuilder $containerBuilder,
+        ): void {
+            // Lädt die Service-Konfiguration des Bundles
+            $containerConfigurator->import('../config/services.yaml');
+        }
     }
