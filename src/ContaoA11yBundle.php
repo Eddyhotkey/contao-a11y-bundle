950391cc81f91834d1c64e49ba14be14d04be5a0
Rashid Bairamov <rashid@s-punkt-online.de>
2025-12-02 09:21:07 +0100

    Fix backend template with bundle Twig namespace

--- src/ContaoA11yBundle.php
+++ src/ContaoA11yBundle.php
@@ -2,20 +2,28 @@
 
     declare(strict_types=1);
 
-    namespace Weba11y\ContaoA11yBundle;
+    namespace Weba11y\ContaoA11yBundle\Controller;
 
-    use Symfony\Component\DependencyInjection\ContainerBuilder;
-    use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-    use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+    use Contao\CoreBundle\Controller\AbstractBackendController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
 
-    class ContaoA11yBundle extends AbstractBundle
+    #[Route(
+        path: '%contao.backend.route_prefix%/a11y-panel',
+        name: 'weba11y_a11y_backend',
+        defaults: ['_scope' => 'backend']
+    )]
+    class A11yBackendController extends AbstractBackendController
     {
-        public function loadExtension(
-            array $config,
-            ContainerConfigurator $containerConfigurator,
-            ContainerBuilder $containerBuilder,
-        ): void {
-            // Lädt die Service-Konfiguration des Bundles
-            $containerConfigurator->import('../config/services.yaml');
+        public function __invoke(): Response
+        {
+            return $this->render(
+                '@ContaoA11y/weba11y_a11y_backend.html.twig',
+                [
+                    'title'     => 'WebA11y Tools',
+                    'headline'  => 'WebA11y – Accessibility Tools',
+                    'introText' => 'Hier kommen später deine Accessibility-Werkzeuge rein.',
+                ]
+            );
         }
     }
